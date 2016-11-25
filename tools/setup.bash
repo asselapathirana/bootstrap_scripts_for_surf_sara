19730b5ecd88fd987944822b175ffdea85077085
Assela Pathirana <assela@pathirana.net>
2016-11-25 13:52:35 +0100

    Add files via upload

---
+++ tools/setup.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+if [ $# -lt 1 ]
+then 
+ 	echo "USAGE: $0 <mother_machine_ip>"
+	exit 1
+fi 
+ip=${1}
+#scp -o "StrictHostKeyChecking no" ubuntu@$ip:/ 
+cat /etc/fstab |awk '{if($3 !="nfs"){ print $0}}' > tmp.file
+cp tmp.file /etc/fstab
+cat << EOF >> /etc/fstab
+$ip:/opt /opt nfs defaults 0 0 
+$ip:/data /data nfs defaults 0 0 
+EOF
+if [ ! -d "/opt" ] ; then mkdir /opt ; fi 
+if [ ! -d "/data" ] ; then mkdir /data ; fi
+
+apt install nfs-common 
+mount -a
+cp /opt/tools/authorized_keys ~/.ssh/.
+chmod og-rwx ~/.ssh/authorized_keys
+apt-get install libboost-all-dev
+. /opt/src/setintel
